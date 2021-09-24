d35533c22dd0d1275a17258e6e05889076734c20
GreenNeptune <silentwisdom75@gmail.com>
2021-09-24 14:49:37 +0300

    add countries json data for chart

--- src/components/chart/chartItem.js
+++ src/components/chart/chartItem.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import ChartItemType from './chartItemType';
+import countriesJSON from '../../json/countries.json';
+
 
 const data = {
   labels: ['1', '2', '3', '4', '5', '6'],
   datasets: [
     {
-      label: '# of Votes',
+      label: countriesJSON[0].name,
       data: [12, 19, 3, 5, 2, 3],
       fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgba(255, 99, 132, 0.2)',
+      backgroundColor: countriesJSON[0].color,
+      borderColor: countriesJSON[0].color,
     },
   ],
 };
