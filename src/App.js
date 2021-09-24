a3513675ffd19876662316657e56aa57abea9040
GreenNeptune <silentwisdom75@gmail.com>
2021-09-24 12:38:10 +0300

    add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './components/header/header';
 import { GlobalContextProvider } from './context/globalContext';
 function App() {
 
   return (
     <div className="App">
       <GlobalContextProvider>
-
+        <Header />
       </GlobalContextProvider>
     </div>
   );
---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header(props) {
+
+  return (
+    <header className="container">
+      header
+    </header>
+  )
+}
+
+export default Header
