04c89b038d283038907c3ba6f21ef106a3b3c313
GreenNeptune <silentwisdom75@gmail.com>
2021-09-24 12:40:44 +0300

    add Menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Menu from '../menu/menu'
 
 function Header(props) {
 
   return (
     <header className="container">
-      header
+      <Menu />
     </header>
   )
 }
---
+++ src/components/menu/menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+function Menu(props) {
+
+  return (
+    <menu>
+      menu
+    </menu>
+  )
+}
+
+
+
+
+export default Menu
