ea04e5abd2d1007e51214ae12d8b446d6910fec9
GreenNeptune <silentwisdom75@gmail.com>
2021-09-24 12:09:51 +0300

    add date utils

---
+++ src/utils/date.js
@@ -0,0 +1,39 @@
+exports.daysFromNow = n => {
+  let d = new Date();
+  d.setDate(d.getDate() + Math.abs(n));
+  return d;
+
+};
+
+exports.today = () => {
+  let d = new Date();
+  return d;
+
+};
+
+exports.yesterday = () => {
+  let d = new Date();
+  d.setDate(d.getDate() - 1);
+  return d;
+
+};
+
+exports.lastWeekend = () => {
+  let d = new Date();
+  d.setDate(d.getDate() - 7);
+  return d;
+
+};
+
+exports.lastMonth = () => {
+  let d = new Date();
+  d.setMonth(d.getMonth() - 1);
+  return d;
+
+};
+
+exports.addDaysToDate = (date, n) => {
+  let d = new Date(date);
+  d.setDate(d.getDate() + n);
+  return d;
+};
